90fe82a681576f758594919051947b0848ec653f
Tanjil <tanjilkh@gmail.com>
2024-06-14 15:08:12 +0100

    fix: misstypo find() carro controller

--- controllers/carro.controller.js
+++ controllers/carro.controller.js
@@ -2,7 +2,7 @@ const models = require('../models')
 const Carro = models.Carro
 
 const getCarros = async(req, res) => {
-    const carros = await Carro.find({})
+    const carros = await Carro.findAll()
     res.json(carros)
 }
 
