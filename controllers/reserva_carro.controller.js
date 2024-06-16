f7bec3b8cf233b458972a5e93a7befd03be8c818
Tanjil <tanjilkh@gmail.com>
2024-06-16 14:54:35 +0100

    reservacarro nested include

--- controllers/reserva_carro.controller.js
+++ controllers/reserva_carro.controller.js
@@ -1,8 +1,38 @@
 const models = require('../models')
 const ReservaCarro = models.ReservaCarro
+const Cliente = models.Cliente
+const Carro = models.Carro
+const Modelo = models.Modelo
+const Marca = models.Marca
 
 const getReservas = async(req, res) => {
-    const reservas = await ReservaCarro.findAll()
+    const reservas = await ReservaCarro.findAll({
+        include: [
+            {
+                model: Cliente,
+                as: 'cliente',
+                attributes: ['nome']
+            },
+            {
+                model: Carro,
+                as: 'carro',
+                attributes: ['id'],
+                include: [
+                    {
+                        model: Marca,
+                        as: 'marca',
+                        attributes: ['nome']
+                    },
+                    
+                    {
+                        model: Modelo,
+                        as: 'modelo',
+                        attributes: ['nome']
+                    }
+                ]
+            }
+        ]
+    })
     res.json(reservas)
 }
 
