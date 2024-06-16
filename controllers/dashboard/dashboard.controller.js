15c815c218aee47e2ae55761cf15f9ef07280af5
Tanjil <tanjilkh@gmail.com>
2024-06-16 16:48:40 +0100

    display cars

--- controllers/dashboard/dashboard.controller.js
+++ controllers/dashboard/dashboard.controller.js
@@ -1,11 +1,32 @@
 const models = require('../../models')
 const Cliente = models.Cliente
+const Carro = models.Carro
+const Marca = models.Marca
+const Modelo = models.Modelo
 
 const getClientes = async(req, res) => {
     const clientes = await Cliente.findAll({
         limit: 10
     })
     return clientes
+}
+
+const getCarros = async(req, res) => {
+    const carros = await Carro.findAll({
+        include: [
+            {
+                model: Marca,
+                as: 'marca',
+                attributes: ['nome']
+            },
+            {
+                model: Modelo,
+                as: 'modelo',
+                attributes: ['nome']
+            }
+        ]
+    })
+    return carros
 }
 
 const renderDashboard = async(req, res) => {
@@ -21,13 +42,26 @@ const renderDashboard = async(req, res) => {
             })
         }
 
-        console.log(clientes_result.result)
+        const carros = await getCarros()
+        let carros_result = {error: "", result: []}
+        for(let i in carros){
+            carros_result.result.push({
+                id: carros[i].id,
+                ano: carros[i].ano,
+                preco: carros[i].preco,
+                marca: carros[i].marca.nome,
+                modelo: carros[i].modelo.nome
+            })
+        }
+
+        console.log(carros_result)
 
         res.render('dashboard', {
             is_admin: req.session.is_admin,
             nome_cliente: req.session.nome_cliente,
             foto_perfil: req.session.foto_perfil,
-            clientes: clientes_result.result
+            clientes: clientes_result.result,
+            carros: carros_result.result
 
         })
     } else {
