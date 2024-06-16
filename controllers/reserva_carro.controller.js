7db24ff1239e1b3e537fba2df5c1e643c7b0c2d1
Tanjil <tanjilkh@gmail.com>
2024-06-16 13:38:03 +0100

    POST Reservas

--- controllers/reserva_carro.controller.js
+++ controllers/reserva_carro.controller.js
@@ -6,6 +6,13 @@ const getReservas = async(req, res) => {
     res.json(reservas)
 }
 
+const createReserva = async(req, res) => {
+    const data = req.body
+    const reserva = await ReservaCarro.create(data)
+    res.json(reserva)
+}
+
 module.exports = {
-    getReservas
+    getReservas,
+    createReserva
 }
--- routes/reserva_carro.js
+++ routes/reserva_carro.js
@@ -3,5 +3,6 @@ var router = express.Router()
 const reservaController = require('../controllers/reserva_carro.controller')
 
 router.get('/', reservaController.getReservas)
+router.post('/', reservaController.createReserva)
 
 module.exports = router
